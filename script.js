06d0c85792d1097b1acb7b9e80d669f51b21e7e2
art <arthurvat7@gmail.com>
2020-06-17 21:39:12 -0700

    arthurv website v2
    v2

--- script.js
+++ script.js
@@ -42,6 +42,33 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html, body').animate({scrollTop : 0},800);
+}
+$('.scrollTo').click(function(){
+    var getElement = $(this).attr('href');
+    if($(getElement).length){
+        var getOffset = $(getElement).offset().top;
+        $('html,body').animate({
+            scrollTop:getOffset
+        }, 1000);
+        }
+        return false;
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  
 }
