1f3c27b00522395c4184da62fcfe197792daf9c1
art <arthurvat7@gmail.com>
2020-06-22 20:30:26 -0700

    v2.2

--- script.js
+++ script.js
@@ -4,85 +4,78 @@ var showwavydescbutton = document.getElementById("showwavydescription");
 var wavydesc = document.getElementById("wavydescription");
 
 
-  $(document).ready(function() {
-    $('#view_details').on('click', function(){
-        $('#hidden').toggle();
-    
-    });
+function openNav() {
+  document.getElementById("mySidenav").style.width = "150px";
+
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  $('.menu-toggler').hide();
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+
+  document.body.style.backgroundColor = "white";
+  $('.menu-toggler').show();
+}
+
+$(document).ready(function () {
+  $('#view_details').on('click', function () {
+    $('#hidden').toggle();
+
+  });
 });
-  $("#showadultingappdescription").click(function(){
-    $("#adultingappdescription").toggle();
-  });
-  $("#showmusicwebsitedescription").click(function(){
-    $("#musicwebsitedescription").toggle();
-  });
-  $("#showweatherappdescription").click(function(){
-    $("#weatherappdescription").toggle();
-  });
-  $("#showsnakegamedescription").click(function(){
-    $("#snakegamedescription").toggle();
-  });
-  $("#showlavalampappdescription").click(function(){
-    $("#lavalampappdescription").toggle();
-  });
-
-
-
-
-
-var act = $('.navbar').hasClass('open');
-$(document).ready(function(){
-    
-
-    $('.menu-toggler').on('click', function () {
-        
-        if(!act){
-            
-            $(this).toggleClass('open');
-        $('.navbar').toggleClass('open');
-        }
-        
-        
-       
-
-    });
-    
+$("#showadultingappdescription").click(function () {
+  $("#adultingappdescription").toggle();
+});
+$("#showmusicwebsitedescription").click(function () {
+  $("#musicwebsitedescription").toggle();
+});
+$("#showweatherappdescription").click(function () {
+  $("#weatherappdescription").toggle();
+});
+$("#showsnakegamedescription").click(function () {
+  $("#snakegamedescription").toggle();
+});
+$("#showlavalampappdescription").click(function () {
+  $("#lavalampappdescription").toggle();
 });
 
-var navbar = document.getElementById('navigationbar');
-navbar.addEventListener("mouseout", function(){
-    navbar.style.background = "transparent";
-})
-navbar.addEventListener("mouseover", function(){
-    navbar.style.background = "rgba(0,0, 0, 0.5)";
-})
+
+
+
+
+
+
+
+
+
 //Get the button:
-mybutton = document.getElementById("myBtn");
+mybutton = document.getElementById("toTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    $("#toTop").fadeOut();
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    $('html, body').animate({scrollTop : 0},800);
+  $('html, body').animate({ scrollTop: 0 }, 800);
 }
-$('.scrollTo').click(function(){
-    var getElement = $(this).attr('href');
-    if($(getElement).length){
-        var getOffset = $(getElement).offset().top;
-        $('html,body').animate({
-            scrollTop:getOffset
-        }, 1000);
-        }
-        return false;
+$('.scrollTo').click(function () {
+  var getElement = $(this).attr('href');
+  if ($(getElement).length) {
+    var getOffset = $(getElement).offset().top;
+    $('html,body').animate({
+      scrollTop: getOffset
+    }, 1000);
+  }
+  return false;
 });
 
 var slideIndex = 1;
@@ -95,10 +88,10 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length };
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  
+
 }
