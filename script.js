75182d68e0ba3ca06ceb191548165922b39a96d2
art <arthurvat7@gmail.com>
2020-07-11 01:17:08 -0700

    test july 11th

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 AOS.init();
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-window.addEventListener('resize', () => {
+window.addEventListener('load', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
